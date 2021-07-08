fb76c1dee0d85381772acc7db1f53cb4191c20ed
ViktoriiaKinash <87100642+ViktoriiaKinash@users.noreply.github.com>
2021-07-09 00:11:29 +0300

    Update Polozenie.h

--- Lab01/include/Polozenie.h
+++ Lab01/include/Polozenie.h
@@ -1,4 +1,4 @@
 #pragma once
 
 float PolozeniePoczatkowe(); /* function returns a float value; takes no arguments  */
-float Polozenie(); /* function returns a float value; takes no arguments; calculate the position value by calling functions  Czas(), PredkoscPoczatkowa(), Przyspieszenie(), PolozeniePoczatkowe(), i Kwadrat(), Iloczyn(), Suma()  */
+float Polozenie(); /* function returns a float value; takes no arguments; calculate the position value by calling functions  Czas(), PredkoscPoczatkowa(), Przyspieszenie(), PolozeniePoczatkowe(), and Kwadrat(), Iloczyn(), Suma()  */
